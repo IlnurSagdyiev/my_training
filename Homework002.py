9195c462483fded9814b396070241de13bed6bbc
IlnurSagdyiev <sagdyiev@live.ru>
2024-09-03 21:38:31 +0300

    Add files via upload

---
+++ Homework002.py
@@ -0,0 +1,5 @@
+completed_hw = 12
+hours_spent = 1.5
+course = "Python"
+time_for_task = hours_spent/completed_hw
+print("Курс: ", course, ", всего задач:", completed_hw, ", затрачено часов: ", hours_spent, ", среднее время выполнения ", time_for_task, " часа.", sep='')
